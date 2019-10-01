c073f211b078cf46ea54b6112803c89bc2fb89fc
Thomas Horner <thomasehorner@gmail.com>
2019-09-30 22:06:31 -0500

    Landing page completed

--- src/components/Landing/landing_components/Skills.js
+++ src/components/Landing/landing_components/Skills.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import { landing_atts } from '../../../data.json';
+import './Skills.css';
+import TextLoop from 'react-text-loop';
+import glamorous from 'glamorous';
 
 export default function Skills() {
-	return <div />;
+	const CenteredText = glamorous.div({ textAlign: 'center', width: 300 });
+	const skills = landing_atts.map(s => {
+		return (
+			<CenteredText>
+				<p>{s}</p>
+			</CenteredText>
+		);
+	});
+
+	return (
+		<div className="skills-view">
+			<TextLoop delay={500}>{skills}</TextLoop>
+		</div>
+	);
 }
